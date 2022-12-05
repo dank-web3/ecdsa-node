366efd373c419d9dea07362d06c065a020f7a795
dank <dank@nightshift.net>
2022-12-05 15:59:25 -0500

    public address
    public address

--- server/scripts/generate.js
+++ server/scripts/generate.js
@@ -1,8 +1,21 @@
 const secp = require("ethereum-cryptography/secp256k1");
 const { toHex } = require("ethereum-cryptography/utils");
+const { keccak256 } = require("ethereum-cryptography/keccak");
 
 const privateKey = secp.utils.randomPrivateKey();
 const publicKey = secp.getPublicKey(privateKey);
 
-console.log("private key: ", toHex(privateKey));
-console.log("public key: ", toHex(publicKey));
+const privateKeyHex = toHex(privateKey);
+const publicKeyHex = toHex(publicKey);
+const publicKeyKeccak256 = keccak256(publicKey);
+
+const publicAddress = toHex(
+  publicKeyKeccak256.slice(
+    publicKeyKeccak256.length - 20,
+    publicKeyKeccak256.length
+  )
+);
+
+console.log("private key: ", privateKeyHex);
+console.log("public key: ", publicKeyHex);
+console.log("publicAddress: ", `0x${publicAddress}`);
